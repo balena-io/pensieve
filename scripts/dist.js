a02e99c2d2782e69801f790496caed88239e9f12
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-08-10 10:35:55 +0100

    Add a build step to produce consistently named static assets
    Connects to #26

---
+++ scripts/dist.js
@@ -0,0 +1,16 @@
+const glob = require('glob-fs')();
+const fs = require('fs');
+
+// Generate consistently named static assets, so that other repos can link to them
+glob.readdirPromise('./build/static/**/*')
+.then((files) => {
+  files.forEach((file) => {
+    const ext = file.split('.').pop();
+    if (ext === 'js' || ext === 'css') {
+      // Replace the build hash with ".latest"
+      const newName = file.replace(/(\.[a-zA-Z0-9]+)(\.(css|js))/, ".latest$2")
+      fs.createReadStream(file)
+      .pipe(fs.createWriteStream(newName));
+    }
+  });
+});
