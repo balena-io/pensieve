47d4aeb41fa7ad1a311066ef6de6e29acf6e6811
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-12-01 11:14:16 +0000

    Normalize imported markdown field values
    Fixes #206
    
    change-type: minor

--- test/importer.spec.js
+++ test/importer.spec.js
@@ -4,6 +4,8 @@ import { expect } from 'chai'
 import * as importer from '../src/services/importer'
 
 const sourceFile = fs.readFileSync('./test/resources/pokedex.md', 'utf8')
+const sourceFile2 = fs.readFileSync('./test/resources/types.md', 'utf8')
+
 const schema = [
   {
     name: 'Name',
@@ -16,6 +18,69 @@ const schema = [
   {
     name: 'Real',
     type: 'Case Insensitive Text'
+  }
+]
+
+const schema2 = [
+  {
+    name: 'Title',
+    type: 'Short Text'
+  },
+  {
+    name: 'Description',
+    type: 'Text'
+  },
+  {
+    name: 'Boolean',
+    type: 'Boolean'
+  },
+  {
+    name: 'Case Insensitive Text',
+    type: 'Case Insensitive Text'
+  },
+  {
+    name: 'Date Time',
+    type: 'Date Time'
+  },
+  {
+    name: 'Date',
+    type: 'Date'
+  },
+  {
+    name: 'Integer',
+    type: 'Integer'
+  },
+  {
+    name: 'Real',
+    type: 'Real'
+  },
+  {
+    name: 'Real',
+    type: 'Real'
+  },
+  {
+    name: 'Short Text',
+    type: 'Short Text'
+  },
+  {
+    name: 'Text',
+    type: 'Text'
+  },
+  {
+    name: 'Time',
+    type: 'Time'
+  },
+  {
+    name: 'Enum',
+    type: 'Enum'
+  },
+  {
+    name: 'Semver Range',
+    type: 'Semver Range'
+  },
+  {
+    name: 'Semver',
+    type: 'Semver'
   }
 ]
 
@@ -58,6 +123,78 @@ describe('importer', () => {
     it('should add preliminary text copy using a special key', () => {
       expect(importer.convert(schema, sourceFile)[2]).to.include({
         'Imported copy 1': 'One of my favourites'
+      })
+    })
+
+    it('should correctly import "Boolean" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Boolean: true
+      })
+    })
+
+    it('should correctly import "Case Insensitive Text" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        'Case Insensitive Text': 'Lorem ipsum dolor sit amet'
+      })
+    })
+
+    it('should correctly import "Date Time" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        'Date Time': '12345'
+      })
+    })
+
+    it('should correctly import "Date" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Date: '12345'
+      })
+    })
+
+    it('should correctly import "Integer" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Integer: 1
+      })
+    })
+
+    it('should correctly import "Real" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Real: 2.5
+      })
+    })
+
+    it('should correctly import "Short Text" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        'Short Text': 'Lorem ipsum dolor sit amet'
+      })
+    })
+
+    it('should correctly import "Text" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Text: 'Lorem ipsum dolor sit amet'
+      })
+    })
+
+    it('should correctly import "Time" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Time: '12345'
+      })
+    })
+
+    it('should correctly import "Enum" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Enum: 'Lorem ipsum dolor sit amet'
+      })
+    })
+
+    it('should correctly import "Semver Range" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        'Semver Range': '> 1.0.0'
+      })
+    })
+
+    it('should correctly import "Semver" types', () => {
+      expect(importer.convert(schema2, sourceFile2)[0]).to.include({
+        Semver: '2.0.0'
       })
     })
   })
